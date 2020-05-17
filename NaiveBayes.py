c610efce0225f022e18f348bb75bc10c93ba8768
BobTsang1995 <57320828+BobTsang1995@users.noreply.github.com>
2020-05-17 22:18:04 +0800

    Create NaiveBayes.py

---
+++ NaiveBayes.py
@@ -0,0 +1,8 @@
+"""
+@File    : NaiveBayes.py
+@Time    : 2020-05-17 
+@Author  : BobTsang
+@Software: PyCharm
+@Email   : bobtsang@bupt.edu.cn
+"""
+朴素贝叶斯的思想基础是这样的：对于给出的待分类项，求解在此项出现的条件下各个类别出现的概率，哪个最大，就认为此待分类项属于哪个类别。
