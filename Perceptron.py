f866280c1ae1c39bad2959c919c35f58e025a5b2
BobTsang1995 <57320828+BobTsang1995@users.noreply.github.com>
2020-05-17 19:08:42 +0800

    Create Perceptron.py

---
+++ Perceptron.py
@@ -0,0 +1,15 @@
+"""
+@File    : Perceptron.py
+@Time    : 2020-05-16 
+@Author  : BobTsang
+@Software: PyCharm
+@Email   : bobtsang@bupt.edu.cn
+"""
+import pandas as pd
+import numpy as np
+import time
+from sklearn import datasets
+# from sklearn.model_selection import train_test_split
+# from sklearn.utils import shuffle
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
